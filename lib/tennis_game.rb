af4b412733e4e49789c4f1bee16d7d172d4a192a
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2016-05-03 19:39:34 +0100

    is 40 - love when the player_1 wins three points in a row

--- lib/tennis_game.rb
+++ lib/tennis_game.rb
@@ -4,14 +4,23 @@ class TennisGame
   end
 
 	def score
-		points_of(:player_1) + ' - ' + points_of(:player_2)
+		format_points_of(:player_1) + ' - ' + format_points_of(:player_2)
 	end
 	
 	def add_point_to player
 		@score[player.to_sym] += 15
 	end
 
-	def points_of player
-		@score[player] == 0 ? 'love' : @score[player].to_s
+	def format_points_of player
+		format = {0 => 'love', 
+							15 => '15', 
+							30 => '30', 
+							45 => '40'}
+		format[@score[player]]
 	end
+
+	def format points
+
+	end
+
 end
