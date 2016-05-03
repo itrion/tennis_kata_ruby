d2c8b321843979efd17c1933eb0e918cd7a03123
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2016-05-03 18:52:08 +0100

    Tennis score is love - 30 when the player_2 wins two points in a row

--- lib/tennis_game.rb
+++ lib/tennis_game.rb
@@ -1,13 +1,17 @@
 class TennisGame
-  def initialize
-		@score = 'love - love'	
+	def initialize
+		@score = {player_1: 0, player_2: 0}
   end
 
 	def score
-		@score
+		points_of(:player_1) + ' - ' + points_of(:player_2)
 	end
 	
 	def add_point_to player
-     @score = 'love - 15'
+		@score[player.to_sym] += 15
+	end
+
+	def points_of player
+		@score[player] == 0 ? 'love' : @score[player].to_s
 	end
 end
--- spec/tennis_score_spec.rb
+++ spec/tennis_score_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe 'Tennis Score' do
 		game.add_point_to 'player_2'
 		expect(game.score).to eq 'love - 15'
 	end
+
+	it 'is love - 30 when the player_2 wins two points in a row' do
+		game = TennisGame.new
+
+		game.add_point_to 'player_2'
+		game.add_point_to 'player_2'
+
+		expect(game.score).to eq 'love - 30'
+	end
 end
